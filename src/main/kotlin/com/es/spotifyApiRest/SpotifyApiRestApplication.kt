bb688cea6922b9613bc933e027d82d71502d1003
sbcesar <148891359+sbcesar@users.noreply.github.com>
2024-12-10 11:44:51 +0100

    First Commit

---
+++ src/main/kotlin/com/es/spotifyApiRest/SpotifyApiRestApplication.kt
@@ -0,0 +1,11 @@
+package com.es.spotifyApiRest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotifyApiRestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpotifyApiRestApplication>(*args)
+}
---
+++ src/test/kotlin/com/es/spotifyApiRest/SpotifyApiRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.es.spotifyApiRest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpotifyApiRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
